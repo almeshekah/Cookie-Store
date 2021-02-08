15f5ad2104a55d93bad92535ad39e58554654a01
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-02-08 11:39:58 +0300

    ok

--- db/models/Product.js
+++ db/models/Product.js
@@ -2,9 +2,9 @@ const { DataTypes } = require("sequelize/types");
 const { sequelize } = require(".");
 
 module.exports=(sequelize,DataTypes)=>{
- return (sequelize.define("Product", {
+ return sequelize.define("Product", {
     name:{ type : DataTypes.STRING },
     price:{ type : DataTypes.INTEGER },
     description : {type : DataTypes.STRING },
 
-}))};
+})};
